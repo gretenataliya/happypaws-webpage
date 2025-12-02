b89d1a1b0b7ad253de5b10a87dd53df4d5e35e6a
jennie johansson <jenniejohanssonretouch@gmail.com>
2025-12-02 14:59:55 +0100

    changed structure in assets folder

---
+++ assets/products.ts
@@ -0,0 +1,92 @@
+const products = [
+    {
+        name: "Bacon Treats",
+        imgUrl: 
+        category: "eat",
+        price: 99
+    },
+    {
+        name: "Big Bones",
+        img:
+        category: "eat",
+        price: 99
+    },
+    {
+        name: "Black Collar",
+        img:
+        category: "walk",
+        price: 99
+    },
+    {
+        name: "Black Lead",
+        img:
+        category: "walk",
+        price: 99
+    },
+    {
+        name: "Chew Treat",
+        img:
+        category: "eat",
+        price: 99
+    },
+    {
+        name: "Lamb Strips",
+        img:
+        category: "eat",
+        price: 99
+    },
+    {
+        name: "Leopard Collar",
+        img:
+        category: "walk",
+        price: 99
+    },
+    {
+        name: "Mini Bones",
+        img:
+        category: "eat",
+        price: 99
+    },
+    {
+        name: "Mint Lead",
+        img:
+        category: "walk",
+        price: 99
+    },
+    {
+        name: "Owl Plushie",
+        img:
+        category: "sleep",
+        price: 99
+    },
+    {
+        name: "Pink Collar",
+        img:
+        category: "walk",
+        price: 99
+    },
+    {
+        name: "Plaid Bed",
+        img:
+        category: "sleep",
+        price: 99
+    },
+    {
+        name: "Star Bed",
+        img:
+        category: "sleep",
+        price: 99
+    },
+    {
+        name: "Striped Bed",
+        img:
+        category: "sleep",
+        price: 99
+    },
+    {
+        name: "Striped Mattress",
+        img:
+        category: "sleep",
+        price: 99
+    }
+]
